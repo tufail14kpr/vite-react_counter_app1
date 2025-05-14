9fe5bb782e85e9e5e9d611cef431896437cef5be
tufail14kpr <tufail4ktr@gmail.com>
2025-05-14 22:31:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React course learn with hitesh</h1>
+      <h2>counter value:</h2>
+      <button>Add value</button>
+    </>
+  )
+}
+
+export default App
